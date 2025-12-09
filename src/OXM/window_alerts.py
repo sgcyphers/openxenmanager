686a59c86c9a2eb8eee7ca47e57228fe5c51f145
Steve Cyphers <66352073+sgcyphers@users.noreply.github.com>
2025-12-08 23:51:49 -0600

    window_alerts.py updated for Python3/PyGObject
    Upgrade OpenXenManager to use Python3 and PyGObjects

--- src/OXM/window_alerts.py
+++ src/OXM/window_alerts.py
@@ -1,12 +1,17 @@
 # -----------------------------------------------------------------------
 # OpenXenManager
 #
-# Copyright (C) 2009 Alberto Gonzalez Rodriguez alberto@pesadilla.org
-# Copyright (C) 2014 Daniel Lintott <daniel@serverb.co.uk>
+#      window_alerts.py
+#
+#     Copyright (C) 2009 Alberto Gonzalez Rodriguez alberto@pesadilla.org
+#     Copyright (C) 2014 Daniel Lintott <daniel@serverb.co.uk>
+#
+#     OpenXenManager application updated to use Python3 and PyGObject - 2025-12-08
+#     Copyright (C) 2025 Steve Cyphers <cthree at c3cyphers.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
+# as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -21,7 +26,11 @@
 # -----------------------------------------------------------------------
 from os import path
 import utils
-import gtk
+
+#    Python3+ uses PyGObjact (gi); GTK2 is obsolete
+import gi
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk
 
 
 class oxcWindowAlerts:
