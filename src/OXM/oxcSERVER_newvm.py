76d9f633eee4b2e16f2c1009ddc95b914973fd2d
Steve Cyphers <66352073+sgcyphers@users.noreply.github.com>
2025-12-08 23:21:06 -0600

    oxcSERVER_newvm.py updated for Python3/PyGObject
    Upgrade OpenXenManager to use Python3 and PyGObjects

--- src/OXM/oxcSERVER_newvm.py
+++ src/OXM/oxcSERVER_newvm.py
@@ -1,8 +1,12 @@
 # -----------------------------------------------------------------------
 # OpenXenManager
 #
-# Copyright (C) 2009 Alberto Gonzalez Rodriguez alberto@pesadilla.org
-# Copyright (C) 2014 Daniel Lintott <daniel@serverb.co.uk>
+#
+#     Copyright (C) 2009 Alberto Gonzalez Rodriguez alberto@pesadilla.org
+#     Copyright (C) 2014 Daniel Lintott <daniel@serverb.co.uk>
+#
+#     OpenXenManager application updated to use Python3 and PyGObject - 2025-12-08
+#     Copyright (C) 2025 Steve Cyphers <cthree at c3cyphers.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -20,8 +24,13 @@
 # USA.
 #
 # -----------------------------------------------------------------------
-import gtk
 import xml.dom.minidom
+
+#    Python3+ uses PyGObjact; GTK2 is obsolete
+import gi
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk
+
 
 from OXM.utils import bytes_to_gb
 
